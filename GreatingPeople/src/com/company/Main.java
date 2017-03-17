1f1bea82b13b229e082bd2fb47959da6aac7a56b
Alexey Moshkov <al.moshkov@mail.ru>
2017-03-17 13:24:40 +0300

    Change in project, because of request "I'm fine!"

--- GreatingPeople/src/com/company/Main.java
+++ GreatingPeople/src/com/company/Main.java
@@ -2,5 +2,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 	System.out.println("How are you doing?");
+        System.out.println("I'm fine, tahnks!");
     }
 }
